b225710a18f028cdc9f49f4961d49ab8df3eccfa
ILYA <Desantt@mail.ru>
2023-02-21 17:31:52 +0300

    Add all example

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трёх значное число");
+
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length == 3)
+{
+    int result = (Value / 10) % 10;
+    Console.WriteLine("Вторая цифра = " + result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёх значное число");
+}
